4945fee777c714279af7b2aaf76ac643548d659a
Renan Alves <57773194+rnads@users.noreply.github.com>
2021-01-22 08:52:40 -0300

    Use Carbon Profile

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Hash;
